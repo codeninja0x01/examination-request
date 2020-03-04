28945bd15f972f58a9daadce09827754885a5dec
Robel Meisho <robel2010boy@gmail.com>
2020-03-04 14:07:56 +0300

    vuex config

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        Clinic Examination Request
-      </div>
-
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">Clinic Examination Request</div>
     </v-app-bar>
 
     <v-content>
-      <ExaminationRequest/>
+      <ExaminationRequest />
+      <ImagingRequest />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import ExaminationRequest from './components/ExaminationRequest';
+import ExaminationRequest from "./components/ExaminationRequest";
+import ImagingRequest from "./components/ImagingRequest";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     ExaminationRequest,
+    ImagingRequest
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import Vuex from "vuex";
-import store from "./store";
+import store from "./store/index";
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
