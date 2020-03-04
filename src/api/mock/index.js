e699892928458a88b260da545fbe8e43ebb1642d
Robel Meisho <robel2010boy@gmail.com>
2020-03-04 14:06:56 +0300

    Create mock data for imaging and patient condition

--- src/api/mock/index.js
+++ src/api/mock/index.js
@@ -1,19 +1,27 @@
-import tests from './data/tests'
-import specimentType from './data/speciment_type'
+import imaging_tests from "./data/imaging_tests";
+import patient_condition from "./data/patient_conditions";
+import tests from "./data/tests";
+import specimentType from "./data/speciment_type";
 
 const fetch = (mockData, time = 0) => {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     setTimeout(() => {
-      resolve(mockData)
-    }, time)
-  })
-}
+      resolve(mockData);
+    }, time);
+  });
+};
 
 export default {
-  fetchTests () {
-    return fetch(tests, 1000) // wait 1s before returning tests
+  fetchTests() {
+    return fetch(tests, 10000); // wait 1s before returning tests
   },
-  fetchSpecimentType () {
-    return fetch(specimentType, 1000) // wait 1s before returning tests
+  fetchSpecimentType() {
+    return fetch(specimentType, 10000); // wait 1s before returning tests
+  },
+  fetchPatientConditions() {
+    return fetch(patient_condition, 10000); // wait 1s before returning tests
+  },
+  fetchImagingTests() {
+    return fetch(imaging_tests, 10000); // wait 1s before returning tests
   }
-}
+};
